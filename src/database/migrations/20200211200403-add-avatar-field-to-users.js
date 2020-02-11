267a17fab3de8efeac53886d979b739424d5a348
Andre Costa <mr.luizandre@gmail.com>
2020-02-11 17:15:39 -0300

    Associate user avatar

---
+++ src/database/migrations/20200211200403-add-avatar-field-to-users.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE', // fazer com que alteração também ocorra dentro da tabela de usuários,
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
