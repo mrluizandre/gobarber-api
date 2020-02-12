e7d389ebcd18317473482c79553756e3f9d0d5b5
Andre Costa <mr.luizandre@gmail.com>
2020-02-12 17:59:59 -0300

    Appointment validations

--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,24 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
 
 routes.use(authMiddleware); // Obedece a ordem das funções. Só é executados nas rotas abaixo
-routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 routes.get('/providers', ProviderController.index);
+routes.post('/files', upload.single('file'), FileController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
