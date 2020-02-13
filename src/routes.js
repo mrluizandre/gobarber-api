e3c012a0c5f6080b152282f457a82fa019b9aa5d
Andre Costa <mr.luizandre@gmail.com>
2020-02-13 10:38:36 -0300

    Provider schedule

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +18,7 @@ routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware); // Obedece a ordem das funções. Só é executados nas rotas abaixo
+routes.get('/schedule', ScheduleController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/providers', ProviderController.index);
